834f12453965051b223509166d390a811b5cf256
Mammon-github <1540321026@qq.com>
2022-06-26 15:12:07 +0800

    添加了一个基本的人物控制，大家可以在场景中游玩了

---
+++ Assets/models/player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private CharacterController cc;
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private float horizontalMove, verticalMove;
+    private Vector3 dir;
+
+    public float gravity;
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask groundLayer;
+    public bool isGround;
+
+    private void Start () {
+        cc = GetComponent<CharacterController> ();
+    }
+
+    private void Update () {
+        isGround = Physics.CheckSphere (groundCheck.position, checkRadius, groundLayer);
+
+        if (isGround && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
+        horizontalMove = Input.GetAxis ("Horizontal") * moveSpeed;
+        verticalMove = Input.GetAxis ("Vertical") * moveSpeed;
+
+        dir = transform.forward * verticalMove + transform.right * horizontalMove;
+        cc.Move (dir * Time.deltaTime);
+
+        if (Input.GetButtonDown ("Jump") && isGround) {
+            velocity.y = jumpSpeed;
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+        cc.Move (velocity * Time.deltaTime);
+    }
+}
